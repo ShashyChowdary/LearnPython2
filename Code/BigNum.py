a47bd847ee7e599bb16b316a86fc1bc29842ac9f
shashy <shashy.19@gmail.com>
2020-04-22 11:28:04 +0530

    BigNum.py

---
+++ Code/BigNum.py
@@ -0,0 +1,18 @@
+a = int(input("Enter a number : "))
+b = int(input("Enter a number : "))
+c = int(input("Enter a number : "))
+if a>=b and a>=c :
+    print("big is :", a)
+elif b>=c and b>=a:
+     print("Big number is :", b)
+else :
+    print("Big is ", c)
+
+
+
+
+
+
+
+
+

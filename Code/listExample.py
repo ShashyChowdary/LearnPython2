3669f87e902d691515aaa526d47134a240a9e61a
shashy <shashy.19@gmail.com>
2020-05-01 08:42:53 +0530

    listExample.py

---
+++ Code/listExample.py
@@ -0,0 +1,11 @@
+a = [6,2,3,0]
+b = [1,7,5,]
+c=a+b
+#print (sorted(a+b)) #,reverse=True))
+# print(sorted(a+b, reverse=True))
+# a.append(b)
+# print(a)
+print (c)
+c.reverse()
+print(c)
+print(sum(c))
